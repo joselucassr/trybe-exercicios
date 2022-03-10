555cef27e9a9846bb74d20581dc6261d401b86f2
José Lucas R. Santos <joselucasrs@gmail.com>
2022-03-10 15:26:48 -0300

    Exercícios 4 e 5 feitos.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte1.js
@@ -6,21 +6,51 @@ let info = {
 
 const exercicio1 = ({ personagem } = info) => {
   console.log(`Bem vinda, ${personagem}`);
-};
 
+  console.log('');
+};
 exercicio1();
 
 const exercicio2 = (obj) => {
   obj.recorrente = 'Sim';
 };
-
 exercicio2(info);
 console.log(info);
+console.log('');
 
 const exercicio3 = (obj) => {
   for (chave in obj) {
     console.log(chave);
   }
+
+  console.log('');
+};
+exercicio3(info);
+
+const exercicio4 = (obj) => {
+  for (chave in obj) {
+    console.log(obj[chave]);
+  }
+
+  console.log('');
+};
+exercicio4(info);
+
+let infoPatinhas = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
 };
 
-exercicio3(info);
+const exercicio5 = (obj1, obj2) => {
+  for (chave in obj1) {
+    console.log(
+      obj1[chave] !== obj2[chave]
+        ? `${obj1[chave]} e ${obj2[chave]}`
+        : `Ambos ${chave}s`,
+    );
+  }
+  console.log('');
+};
+exercicio5(info, infoPatinhas);
