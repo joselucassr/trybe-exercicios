fbc2962327cad84ea81b279c3173e3991b9a8293
José Lucas R. Santos <joselucasrs@gmail.com>
2022-03-18 20:54:27 -0300

    Estilos de tamanho de fonte de altura de linha feitos.

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/scripts.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/scripts.js
@@ -37,3 +37,50 @@ let blackOverlay = document.querySelector('#overlay');
 function invertColors() {
   blackOverlay.classList.toggle('hidden');
 }
+
+let articleTag = document.querySelector('article');
+
+let baseFontSize = '1.20rem';
+articleTag.style.fontSize = baseFontSize;
+
+let baseLineHeight = '1.60rem';
+articleTag.style.lineHeight = baseLineHeight;
+
+let smallerTextBtn = document.querySelector('.smallerText');
+let largerTextBtn = document.querySelector('.largerText');
+let shorterLineBtn = document.querySelector('.shorterLine');
+let tallerLineBtn = document.querySelector('.tallerLine');
+
+smallerTextBtn.addEventListener('click', changeArticleStyle);
+largerTextBtn.addEventListener('click', changeArticleStyle);
+shorterLineBtn.addEventListener('click', changeArticleStyle);
+tallerLineBtn.addEventListener('click', changeArticleStyle);
+
+let textSizeLabel = document.querySelector('.textSize');
+textSizeLabel.innerText = baseFontSize;
+
+let lineHeightLabel = document.querySelector('.lineHeight');
+lineHeightLabel.innerText = baseLineHeight;
+
+function changeArticleStyle(event) {
+  let targetClass = event.target.classList[0];
+  let currentTextSize = parseFloat(articleTag.style.fontSize);
+  let currentLineHeight = parseFloat(articleTag.style.lineHeight);
+
+  if (targetClass === 'smallerText') currentTextSize -= 0.05;
+  if (targetClass === 'largerText') currentTextSize += 0.05;
+  if (targetClass === 'shorterLine') currentLineHeight -= 0.05;
+  if (targetClass === 'tallerLine') currentLineHeight += 0.05;
+
+  currentTextSize = currentTextSize.toFixed(2);
+  currentTextSize = currentTextSize + 'rem';
+
+  currentLineHeight = currentLineHeight.toFixed(2);
+  currentLineHeight = currentLineHeight + 'rem';
+
+  articleTag.style.fontSize = currentTextSize;
+  textSizeLabel.innerText = currentTextSize;
+
+  articleTag.style.lineHeight = currentLineHeight;
+  lineHeightLabel.innerText = currentLineHeight;
+}
