90730a6bd20e12e229ece9b8949ed5cce456653e
José Lucas R. Santos <joselucasrs@gmail.com>
2022-04-02 19:57:48 -0300

    Exercício 2 feito.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte-1/exercicio2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte-1/exercicio2.js
@@ -2,4 +2,4 @@ const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 // Seu código aqui.
 
-console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+console.log(oddsAndEvens.sort((a, b) => a - b)); // será necessário alterar essa linha 😉
