630dfb5662dca51e186c6031c5389df3a054fd39
José Lucas R. Santos <joselucasrs@gmail.com>
2022-04-02 19:52:42 -0300

    Fim do exercício 1.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte-1/exercicio1.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte-1/exercicio1.js
@@ -1,13 +1,6 @@
-function testingScope(escopo) {
-  if (escopo === true) {
-    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-    console.log(ifScope);
-  } else {
-    var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-    console.log(elseScope);
-  }
-  console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
-}
+const testingScope = (escopo) =>
+  escopo
+    ? `Não devo ser utilizada fora do meu escopo (if) ótimo, fui utilizada no escopo !`
+    : `Não devo ser utilizada fora meu escopo (else)`;
 
-testingScope(true);
+console.log(testingScope(true));
