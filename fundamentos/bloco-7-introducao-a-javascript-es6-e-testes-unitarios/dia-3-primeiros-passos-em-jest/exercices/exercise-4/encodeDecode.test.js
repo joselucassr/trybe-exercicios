055e27d09bdefabca1dfdb835238a25bc8950ae4
José Lucas R. Santos <joselucasrs@gmail.com>
2022-04-03 16:44:34 -0300

    Exercício 4.2 feito.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercices/exercise-4/encodeDecode.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercices/exercise-4/encodeDecode.test.js
@@ -9,4 +9,8 @@ describe('Tests the encode and decode functions', () => {
   it('Checks if decode is a function.', () => {
     expect(typeof decode).toBe('function');
   });
+
+  it('Encode returns "12345" when passing: "aeiou"', () => {
+    expect(encode('aeiou')).toBe('12345');
+  });
 });
