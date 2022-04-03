b0875aaff6263491a23edfdff66460a8a51e2126
José Lucas R. Santos <joselucasrs@gmail.com>
2022-04-03 15:57:07 -0300

    Exercício 1.2 feito.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercices/exercice-1/sumEx.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercices/exercice-1/sumEx.test.js
@@ -1,7 +1,12 @@
+const { it, expect } = require('@jest/globals');
 const { sumEx } = require('./sumEx');
 
 describe('Tests sum function', () => {
   it('Returns 9 as the sum of 4 and 5.', () => {
     expect(sumEx(4, 5)).toBe(9);
+  });
+
+  it('Return 0 as the sum of 0 and 0', () => {
+    expect(sumEx(0, 0)).toBe(0);
   });
 });
