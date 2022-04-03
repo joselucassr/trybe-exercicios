85dde99d53287808cc06122f04706e43b17fbe8c
José Lucas R. Santos <joselucasrs@gmail.com>
2022-04-03 16:25:07 -0300

    Exercício 3.3 feito.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercices/exercise-3/myFizzBuzz.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercices/exercise-3/myFizzBuzz.test.js
@@ -1,4 +1,4 @@
-const { describe, expect } = require('@jest/globals');
+const { describe, expect, it } = require('@jest/globals');
 const { myFizzBuzz } = require('./myFizzBuzz');
 
 describe('Tests the function myFizzBuzz', () => {
@@ -8,5 +8,9 @@ describe('Tests the function myFizzBuzz', () => {
 
   it('Returns fizz when passing 9.', () => {
     expect(myFizzBuzz(9)).toBe('fizz');
+  });
+
+  it('Returns buzz when passing 25.', () => {
+    expect(myFizzBuzz(25)).toBe('buzz');
   });
 });
